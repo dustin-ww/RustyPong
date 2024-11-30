755a2e36069388276ceb96e6e9e9569bfb8b660a
Tom <ts2000@live.de>
2024-11-30 14:11:57 +0100

    Added game plan

--- src/bin/game_window.rs
+++ src/bin/game_window.rs
@@ -15,7 +15,7 @@ impl Stick {
     }
 
     fn update() { // What inputs, keyboard inputs or position values? Or do we update somewhere else?
-        todo!();
+        todo!(); screen_height();
     }
 
     fn draw(&self) {
@@ -49,6 +49,46 @@ impl Ball {
     }
 }
 
+struct  GameState {
+    score_left: i32,
+    score_right: i32,
+    is_running: bool,
+}
+
+impl GameState {
+    fn new() -> GameState {
+        GameState {
+
+        }
+    }
+
+}
+
+fn update(gameState: &mut GameState , dt: f32) -> GameState {
+
+    if is_key_down(KeyCode::Down) {
+        gameState.left_stick.update(KeyCode::Down, dt) // Muss sich letzte Position merken
+    } else if is_key_down(KeyCode::Up) {
+        gameState.left_stick.update(KeyCode::Up, dt) // Muss sich letzte Position merken
+    }
+    //TODO Check right stick
+
+    //Check Collision
+    // Ball with stick
+    // balls to the wall
+    // ball with goal --> updateScore
+
+    //checkfinished
+
+}
+
+fn draw(gameState: &mut GameState , dt: f32) -> GameState {
+    //        clear_background(BLACK);
+    //         left_stick.draw();
+    //         right_stick.draw();
+    //         ball.draw();
+    //          overlay.draw();
+}
 
 #[macroquad::main("Pong")]
 async fn main() {
@@ -57,15 +97,18 @@ async fn main() {
     let left_stick = Stick::new(Vec2::new(10.0, 0.0), stick_size); // Height based on screen height? y = screen_height /3 or so?
     let right_stick = Stick::new(Vec2::new(500.0, 0.0), stick_size); // Hard coding the position isn't too good either
 
+    // gameState(ball,left_stick,right_stick)
+
     // TODO - Update the positon of the ball to be in the middle an give an direction
     let ball = Ball::new(Vec2::new((right_stick.position.x - left_stick.position.x) / 2.0 , (right_stick.position.y / 2.0) ), Vec2::new(0.0,0.0), ball_size); // Typesave: Cannot divide a f32 by an i32
-    loop {
-        clear_background(BLACK);
-        left_stick.draw();
-        right_stick.draw();
+    // dt
+    while gameState.is_running {
 
-        ball.draw();
-        // TODO - Where to check if the ball is colliding?
+        //  update(gameState, dt)
+        // draw()
+
+        // Exit when set score or esc
         next_frame().await;
     }
+    // Show winner
 }
